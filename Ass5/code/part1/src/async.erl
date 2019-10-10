367da587b6646dc7679650ba697a0e31aefca718
aashish14002 <aashish14002@iiitd.ac.in>
2019-10-10 15:11:27 +0200

    Add Assignment 5 with warmup solution

---
+++ Ass5/code/part1/src/async.erl
@@ -0,0 +1,63 @@
+-module(async).
+
+-export([new/2, wait/1, poll/1]).
+
+new(Fun, Arg) -> spawn(fun() -> start_supervisor(Fun, Arg) end).
+
+
+wait(Aid) -> 
+	Ref = make_ref(),
+	Me  = self(),
+	Aid ! {send_info, Me, Ref},
+	receive
+		{Ref, {exception, Ex}} -> throw(Ex);
+		{Ref,{ok, Result}} -> Result
+	end.
+
+poll(Aid) -> 
+	Ref = make_ref(),
+	Me = self(),
+	Aid ! {are_you_ready, Me, Ref},
+	receive
+		{Ref, Result} -> Result
+	end.
+
+start_supervisor(Fun, Arg) ->
+	process_flag(trap_exit, true),
+	Super_Pid = self(),
+	Action_Pid = spawn_link(fun() -> 
+				Result=Fun(Arg),
+				Super_Pid ! {result, self(), Result} end),
+	loop_supervisor(Action_Pid,{},[]).
+
+loop_supervisor(Action_Pid, Result_state, Sub_list) -> 
+	receive
+		{'EXIT', Action_Pid, {Ex, _}} -> 
+						send_msg({exception, Ex}, Sub_list),
+						loop_supervisor(Action_Pid, {exception, Ex}, []);
+		{are_you_ready, From, Ref} -> 
+						From ! {Ref,Result_state}, 
+						loop_supervisor(Action_Pid, Result_state, Sub_list);
+		{send_info, From, Ref} ->
+						case Result_state of
+							{ok, Result} -> From ! {Ref, {ok, Result}},
+											loop_supervisor(Action_Pid, Result_state, Sub_list);
+							{exception, Ex} -> From ! {Ref, {exception, Ex}},
+											loop_supervisor(Action_Pid, Result_state, Sub_list);												
+							nothing -> loop_supervisor(Action_Pid, Result_state, [{From, Ref} | Sub_list])
+						end;
+		{result, Action_Pid, Result} -> 
+						send_msg({ok, Result}, Sub_list),
+						loop_supervisor(Action_Pid, {ok, Result}, [])
+
+	end. 
+
+send_msg(_, []) -> nothing;
+send_msg(Result, [{From, Ref}|Tail]) -> From ! {Ref, Result},
+										send_msg(Result, Tail).
+
+
+%% Optional functions, recommended
+
+% wait_catch(Aid) -> nope.
+% wait_any(Aids) -> nope.
