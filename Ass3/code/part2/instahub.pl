56dd1bea4b03c33e25867b675e5fb82eb998e551
keratin7 <arnav2495@gmail.com>
2019-10-16 18:34:30 +0200

    Reordered

--- Ass3/code/part2/instahub.pl
+++ Ass3/code/part2/instahub.pl
@@ -1,12 +1,6 @@
 %% % AP2019 Assignment 3
 % Skeleton for main part. Predicates to implement:
 
-g1([person(kara, [barry, clark]),
- person(bruce, [clark, oliver]),
- person(barry, [kara, oliver]),
- person(clark, [oliver, kara]),
- person(oliver, [kara])]).
-
 % removes X from list and return Rest
 is_selec(X, [Head|Tail], Rest) :-
   is_select3(Tail, Head, X, Rest).
@@ -38,27 +32,12 @@ following([person(X,L)|_], X, L).
 following([_|T], X, L):-
 	following(T, X, L).
 
-%%% level 0 %%%
-
-follows([person(X,D)|_],X,Y):-
-	membe(Y,D).
-follows([_|AS],X,Y):-
-	follows(AS,X,Y).
-    
-% ignores(G, X, Y)
-ignores(G,X,Y):-
-	does_ignore(G,G,X,Y).
-
-does_ignore(N,[person(X,F)|_],X,Y):-
-	follows(N,Y,X), 
-	not_membe(N,Y,F).
-does_ignore(G,[_|AS],X,Y):- 
-	does_ignore(G,AS,X,Y).
-
+%% List of all people in graph.
 list_people([], []).
 list_people([person(P,_)|T], [P|List]) :-
 	list_people(T, List).
 
+%% Concatenate two lists
 concat(L1, [], L1).
 concat(L1, [E|L2], [E|L3]) :-
 	concat(L1, L2, L3).
@@ -75,6 +54,33 @@ l_aware(G, [H|T], New_Aware_List, Visited):-
 l_aware(G, [H|T], New_Aware_List, Visited):-
 	membe(H, Visited),
 	l_aware(G, T, New_Aware_List, Visited).
+
+%% checks if Y is ignored by everyone in the list
+ignored_list(_,[],_).
+ignored_list(G,[X|T],Y) :-
+    ignores(G,X,Y),ignored_list(G,T,Y).
+
+%% checks if Y ignores everyone in the list
+ignores_list(_,[],_).
+ignores_list(G,[X|T],Y) :-
+    ignores(G,Y,X),ignores_list(G,T,Y).
+
+%%% level 0 %%%
+
+follows([person(X,D)|_],X,Y):-
+	membe(Y,D).
+follows([_|AS],X,Y):-
+	follows(AS,X,Y).
+    
+% ignores(G, X, Y)
+ignores(G,X,Y):-
+	does_ignore(G,G,X,Y).
+
+does_ignore(N,[person(X,F)|_],X,Y):-
+	follows(N,Y,X), 
+	not_membe(N,Y,F).
+does_ignore(G,[_|AS],X,Y):- 
+	does_ignore(G,AS,X,Y).
 
 %%% level 1 %%%
 
@@ -96,16 +102,6 @@ is_popular(G,[person(X,Y)|_],X) :-
     followed_list(G,Y,X).  %change variable names. X=Y
 is_popular(G,[_|T],X) :-
     is_popular(G,T,X).
-
-%% checks if Y is ignored by everyone in the list
-ignored_list(_,[],_).
-ignored_list(G,[X|T],Y) :-
-    ignores(G,X,Y),ignored_list(G,T,Y).
-
-%% checks if Y ignores everyone in the list
-ignores_list(_,[],_).
-ignores_list(G,[X|T],Y) :-
-    ignores(G,Y,X),ignores_list(G,T,Y).
 
 outcast(G,X):-
     is_outcast(G,G,X).
