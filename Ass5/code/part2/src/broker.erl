05683912035c4b256b6eef9e430ac85ae72d7989
keratin7 <arnav2495@gmail.com>
2019-10-18 15:36:13 +0200

    Broker spawning coord

--- Ass5/code/part2/src/broker.erl
+++ Ass5/code/part2/src/broker.erl
@@ -1,5 +1,6 @@
 -module(broker).
 -behaviour(gen_server).
+-import (coordinator, [start/1]).
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
@@ -17,23 +18,13 @@ init([]) ->
     {ok, #{lm=>Longest, q=>Queue, on=>Ong, df=>DrainFlag}}.
 
 handle_call({Name, Rounds}, P_id, State) ->		% queue_up
-	DF = maps:get(df, State)
-	if(DF =:= true) ->		% Drain
-
-		On = maps:get(on, State),
-		lists:foreach(fun({Id, Ref}) -> Id ! {purge_mothafuckas} end, On),
-		% Wait for all co-ordinators to die.
-
-	true ->
 		Que = maps:get(q, State),
 		Found = maps:is_key(Rounds,Que),
 		if
 		 Found == true ->	%If other player is found
 			{Nm, P1_id} = maps:get(Rounds, Que),	%Get player name, id
-			Que_rm = maps:remove(Rounds, Que),
-			% Cord = woopie,				% Remove player from Que
-			Cord = gen_statem:start_link([,,,]),
-			Cord ! {P_id, P1_id, Rounds}
+			Que_rm = maps:remove(Rounds, Que),	% Remove player from Que
+			Cord = coordinator:start([P1_id,P_id,Rounds,self()]),	% Start coordinator with player_ids
 			gen_server:reply(P1_id, {ok, Name, Cord}),
 			Ong = maps:get(on, State),
 			NOng = lists:append(Ong, [Cord]),
@@ -43,8 +34,7 @@ handle_call({Name, Rounds}, P_id, State) ->		% queue_up
 		true ->
 			Que_wp = maps:put(Rounds, {Name, P_id}, Que),
 			{noreply, maps:put(q, Que_wp, State), infinity}
-		end
-	end;
+		end;
 handle_call(tell, _F, State) ->		% temporary tell
 	{reply, {ok, State}, State};
 handle_call(stats, _From, State)->	% statistics
